b8ad0a9f8044f28589fb9ae94d111dadf97b621f
ardasakarya <sakaryaarda@gmail.com>
2025-05-14 13:54:08 +0300

    hamburger  menu düzenlemesi

--- product.js
+++ product.js
@@ -1,11 +1,21 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     // Mobile menu toggle
     const menuButton = document.querySelector('.ri-menu-line').parentElement;
     const mobileMenu = document.querySelector('.fixed.inset-0.bg-white.z-40');
-    
-    menuButton.addEventListener('click', function() {
+
+    menuButton.addEventListener('click', function () {
         mobileMenu.classList.toggle('hidden');
         mobileMenu.classList.toggle('flex');
+    });
+
+    // Menüdeki tüm itemleri seç (örneğin <a> etiketleri)
+    const menuItems = mobileMenu.querySelectorAll('a');
+
+    menuItems.forEach(item => {
+        item.addEventListener('click', function () {
+            mobileMenu.classList.add('hidden');
+            mobileMenu.classList.remove('flex');
+        });
     });
 });
 
